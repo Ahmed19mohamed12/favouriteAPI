50f3375578362d8afca0d7970e0a4abf79bb77ca
Ahmed Abd El Hady <83279919+Ahmed19mohamed12@users.noreply.github.com>
2023-10-03 19:13:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,24 +4,27 @@ app = Flask(__name__)
 
 @app.route("/api",methods=['GET'])
 def home():
-    d = {}
-    d['Query'] = str(request.args['Query'])
-    yt = YouTube("https://www.youtube.com/watch?v="+str(request.args['Query']))
-
-    # Get the video title and author
-    title = yt.title
-    author = yt.author
-    audio = yt.streams.filter(only_audio=True).first().url
-    publish_date = yt.publish_date
-    duration = yt.length
-    thumbnail_url = yt.thumbnail_url
-    d['title'] = title
-    d['author'] = author
-    d['audio'] = audio
-    d['upload_date'] = publish_date
-    d['duration'] = duration
-    d['thumbnail_url'] = thumbnail_url
-    # Download the highest resolution audio stream available
-    # yt.streams.get_audio_only().download(output_path='.', filename=f'{title} by {author}.mp3')
+    try:
+        d = {}
+        d['Query'] = str(request.args['Query'])
+        yt = YouTube("https://www.youtube.com/watch?v="+str(request.args['Query']))
     
-    return d
+        # Get the video title and author
+        title = yt.title
+        author = yt.author
+        audio = yt.streams.filter(only_audio=True).first().url
+        publish_date = yt.publish_date
+        duration = yt.length
+        thumbnail_url = yt.thumbnail_url
+        d['title'] = title
+        d['author'] = author
+        d['audio'] = audio
+        d['upload_date'] = publish_date
+        d['duration'] = duration
+        d['thumbnail_url'] = thumbnail_url
+        # Download the highest resolution audio stream available
+        # yt.streams.get_audio_only().download(output_path='.', filename=f'{title} by {author}.mp3')
+        
+        return d
+    except Exception as e:
+        return e
