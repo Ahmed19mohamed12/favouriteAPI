8d848be61b5128292dde9181eedb57b1bdfd213e
Ahmed Abd El Hady <83279919+Ahmed19mohamed12@users.noreply.github.com>
2023-10-04 13:21:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,28 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 from pytube import YouTube
+
 app = Flask(__name__)
 
-@app.route("/api",methods=['GET'])
+@app.route("/api", methods=['GET'])
 def home():
-    try:
-        d = {}
-        d['Query'] = str(request.args['Query'])
-        yt = YouTube("https://www.youtube.com/watch?v="+str(request.args['Query']))
+    d = {}
+    d['Query'] = str(request.args['Query'])
+    yt = YouTube("https://www.youtube.com/watch?v="+str(request.args['Query']))
+
+    # Get the video title and author
+    title = yt.title
+    author = yt.author
+    audio = yt.streams.filter(only_audio=True).first().url
+    publish_date = yt.publish_date
+    duration = yt.length
+    thumbnail_url = yt.thumbnail_url
+    d['title'] = title
+    d['author'] = author
+    d['audio'] = audio
+    d['upload_date'] = publish_date
+    d['duration'] = duration
+    d['thumbnail_url'] = thumbnail_url
+    # Download the highest resolution audio stream available
+    # yt.streams.get_audio_only().download(output_path='.', filename=f'{title} by {author}.mp3')
     
-        # Get the video title and author
-        title = yt.title
-        author = yt.author
-        audio = yt.streams.filter(only_audio=True).first().url
-        publish_date = yt.publish_date
-        duration = yt.length
-        thumbnail_url = yt.thumbnail_url
-        d['title'] = title
-        d['author'] = author
-        d['audio'] = audio
-        d['upload_date'] = publish_date
-        d['duration'] = duration
-        d['thumbnail_url'] = thumbnail_url
-        # Download the highest resolution audio stream available
-        # yt.streams.get_audio_only().download(output_path='.', filename=f'{title} by {author}.mp3')
-        
-        return d
-    except Exception as e:
-        print(e)
+    return jsonify(d)
